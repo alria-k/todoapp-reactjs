655234332188ceb294b587ab9112d71bfd41efd6
Alria <artur.kirovich@gmail.com>
2023-03-26 00:46:21 +0600

    added usestate

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,6 +1,21 @@
+import React from "react";
 import { FormList } from "../index";
 
 export function Form() {
+  const [text, setText] = React.useState("");
+  const [todos, setTodos] = React.useState([]);
+
+  const handleChange = (e) => {
+    setText(e.target.value);
+  };
+
+  const handleTodos = () => {
+    if (!text == "") {
+      setTodos([...todos, text]);
+      setText("");
+    }
+  };
+
   return (
     <div className="container">
       <div className="form__container">
@@ -10,13 +25,23 @@ export function Form() {
             <input
               className="form__input"
               type="text"
+              value={text}
               placeholder="Write todo"
+              onChange={handleChange}
             />
-            <button className="form__btn">Add Todo!</button>
+            <button className="form__btn" onClick={handleTodos}>
+              Add Todo!
+            </button>
           </label>
         </div>
         <ul className="form-list__box">
-          <FormList />
+          {todos.length > 0 ? (
+            todos.map((elem, index) => {
+              return <FormList key={index} val={elem} />;
+            })
+          ) : (
+            <div>No Todos</div>
+          )}
         </ul>
       </div>
     </div>
--- src/components/FormList/FormList.jsx
+++ src/components/FormList/FormList.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-export function FormList({ value }) {
+export function FormList({ val }) {
   return (
     <li className="form__list">
       <label className="form__checkbox" htmlFor="form-checkbox">
         <input name="form-checkbox" type="checkbox" />
       </label>
-      <p className="form__text">lol{value}</p>
+      <p className="form__text">{val}</p>
     </li>
   );
 }
